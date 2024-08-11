745c9c3e46bb0452374cd41ed65569ac6517869c
Ishtiaq Ul Haq Syed <ishtiaq156@gmail.com>
2024-08-10 22:07:48 -0400

    initial commit

---
+++ src/main/java/com/ishtiaqsyed/you/YouApplication.java
@@ -0,0 +1,13 @@
+package com.ishtiaqsyed.you;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouApplication.class, args);
+    }
+
+}
